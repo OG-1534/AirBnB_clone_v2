b9ee1e82f0c996514648ac4cce73710b3753bce1
OG-1534 <gershomochanda@gmail.com>
2024-04-24 11:13:13 -0700

    Write a script that starts a Flask web application: Your web application must be listening on 0.0.0.0, port 5000, Routes /: : display “Hello HBNB!”, /hbnb: display “HBNB”, /c/<text>: display “C ” followed by the value of the text variable, /python/<text>: display “Python ”, followed by the value of the text variable, display “n is a number” only if n is an integer, display a HTML page only if n is an integer, option strict_slashes=False in your route definition

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
-"""Script that starts a Flask web application
-web application is listening on 0.0.0.0, port 5000
+""" Script that starts a Flask web application
+Web application is listening on 0.0.0.0, port 5000
 """
-
-from flask import Flask
-
-app = Flask("__name__")
+from flask import Flask, render_template
+app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
 def hello():
     """String to Return"""
-    return ("Hello HBNB!")
+    return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """String to Return"""
-    return ("HBNB")
+    return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route('/c/<text>', strict_slashes=False)
 def cText(text):
-    """display C followed by the value of the text variable
-    replace underscore _ symbols with a space"""
+    """display C followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
-@app.route("/python/<text>", strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
 def pythonText(text="is cool"):
     """display Python, followed by the value of the text variable"""
     return "Python {}".format(text.replace("_", " "))
 
 
-@app.route("/number/<int:n>", strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def isNumber(n):
-    """Display "n is a number" only if n is an integer"""
+    """display n is a number only if n is an integer"""
     if isinstance(n, int):
         return "{} is a number".format(n)
 
